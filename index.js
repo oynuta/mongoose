b9b3570f315a53edd28cd959ccfe83aff18cfbe9
Oymonjon <macsudbekovicmaksadbek@gmail.com>
2025-11-28 19:15:54 +1300

    gou

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
-console
+// import TelegramBot from "node-telegram-bot-api";
+import TelegramBot from "node-telegram-bot-api";
+import { config } from "dotenv";
+import { onStart } from "./src/onStart.js";
+// import { onStart } from "./src/onStart.js";
+// import { onCourses } from "./src/onCourses.js";
+// import { onRegister } from "./src/onRegister.js";
+// import onUsers from "./src/onUsers.js";
+import mongoose from "mongoose";
+config();
+
+const TOKEN = process.env.BOT_TOKEN;
+
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log(`Db is connected successfully...`);
+  })
+  .then(() => {
+    console.log(`Congratulations!`);
+  })
+  .catch(() => {
+    console.log(`Error: db is not connected..!!`);
+  });
+
+bot.on("message", (msg) => {
+  // console.log(msg);
+  const chatId = msg.chat.id;
+  const text = msg.text;
+  const firstName = msg.chat.first_name;
+
+  //   bot.sendMessage(chatId, text);
+
+  if (text == "/start" || text == "Asosiy menyuga qaytish") {
+    onStart(chatId, firstName);
+  } 
+//   else if (text == "📚 Kurslar") {
+//     // onCourses(chatId);
+//   } else if (text == "ℹ️ Markaz haqida") {
+//     bot.sendMessage(chatId, "📍 Bizning o‘quv markaz joylashuvi:");
+//     bot.sendLocation(chatId, 41.3781989, 60.3694056);
+//   } else if (text == "✍️ Ro‘yxatdan o‘tish") {
+//     onRegister(chatId);
+//   } else if (text == "/users") {
+//     onUsers(chatId);
+//   } else {
+//     bot.sendMessage(
+//       chatId,
+//       `
+//     ⚠️ Kechirasiz, men sizning xabaringizni tushunmadim.
+
+// Iltimos, quyidagi tugmani bosing 👇
+// /start
+
+//     `,
+//       {
+//         reply_markup: {
+//           keyboard: [[{ text: `Asosiy menyuga qaytish` }]],
+//           resize_keyboard: true,
+//         },
+//       }
+//     );
+//   }
+});
+
+console.log("Bot ishga tushdi...");
+
+export { bot };
---
+++ src/onStart.js
@@ -0,0 +1,7 @@
+function onStart(chatId, firstname) {
+    console.log("on start...");
+    console.log(`${chatId} ---> ${firstname}`);
+    
+}
+
+export { onStart }
