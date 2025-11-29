f7e510ddf2f42c8cbb1eb9dc5e5b7561a1445e39
Oynuta <rzmetova>
2025-11-29 16:38:20 +0500

    congrats

--- src/onStart.js
+++ src/onStart.js
@@ -1,7 +1,17 @@
-function onStart(chatId, firstname) {
-    console.log("on start...");
-    console.log(`${chatId} ---> ${firstname}`);
-    
-}
+import { bot } from "../index.js";
 
-export { onStart }
+export function onStart(chatId, firstName) {
+  bot.sendMessage(
+    chatId,
+    `Salom, ${firstName}! O‘quv markazimizga xush kelibsiz. Quyidagi bo‘limlardan birini tanlang:`,
+    {
+      reply_markup: {
+        keyboard: [
+          ["📚 Kurslar", "ℹ️ Markaz haqida"],
+          ["✍️ Ro‘yxatdan o‘tish"]
+        ],
+        resize_keyboard: true
+      }
+    }
+  );
+}
